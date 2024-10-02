5b60c947757afb0523bc8287147620905e967a00
SarahMajed <programmerSara1@gmail.com>
2024-10-02 08:49:01 +0300

    bookmodule

--- bookmodule/urls.py
+++ bookmodule/urls.py
@@ -13,4 +13,11 @@ urlpatterns = [
     path('index2/<int:val1>/', views.index2),
     path('<int:bookId>', views.viewbook)
 ]
+urlpatterns = [
+    path('', views.index5, name= "books.index5"),
+    path('list_books/', views.list_books, name= "books.list_books"),
+    path('<int:bookId>/', views.viewbook, name="books.view_one_book"),
+    path('aboutus/', views.aboutus, name="books.aboutus"),
+    path('books/', views.one_book, name="books.one_book"),
+]
 
--- bookmodule/views.py
+++ bookmodule/views.py
@@ -9,7 +9,7 @@ def index1(request):
 
 def index(request):
     name = request.GET.get("name") or "world!"
-    #return render(request, "index1.html", {"name": name})  
+    return render(request, "index1.html", {"name": name})  
 
     return HttpResponse("Hello, " + name)
  
@@ -25,3 +25,18 @@ def viewbook(request, bookId):
     if book2['id'] == bookId: targetBook = book2
     context = {'book':targetBook} # book is the variable name accessible by the template
     return render(request, 'show.html', context)
+
+def index5(request):
+ return render(request, 'index5.html')
+
+def list_books(request):
+ return render(request, 'list_books.html')
+
+def viewbook(request, bookId):
+ return render(request, 'one_book.html')
+
+def aboutus(request):
+ return render(request, 'aboutus.html')
+
+def one_book(request):
+ return render(request, 'one_book.html')
